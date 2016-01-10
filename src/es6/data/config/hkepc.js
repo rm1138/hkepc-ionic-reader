e20e74bc25c176c0ba223b6df57eb8d0c9da89b9
Gary Lo <logap@me.com>
2016-01-11 02:00:03 +0800

    web proxy config

--- src/es6/data/config/hkepc.js
+++ src/es6/data/config/hkepc.js
@@ -4,12 +4,12 @@
 module.exports = {
   domain: 'http://www.hkepc.com/',
   forum: {
-    image:'http://www.hkepc.com/forum/',
-    index: 'http://www.hkepc.com/forum/index.php',
-    topics: (topicId,page) => `http://www.hkepc.com/forum/forumdisplay.php?fid=${topicId}&page=${page}`,
-    posts: (topicId,postId,page) => `http://www.hkepc.com/forum/viewthread.php?fid=${topicId}&tid=${postId}&page=${page}`,
-    login: 'http://www.hkepc.com/forum/logging.php?action=login&loginsubmit=yes&floatlogin=yes&inajax=1',
-    reply: (topicId,postId) => `http://www.hkepc.com/forum/post.php?action=reply&fid=${topicId}&tid=${postId}&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1`
+    image:'/forum/',
+    index: '/forum/index.php',
+    topics: (topicId,page) => `/forum/forumdisplay.php?fid=${topicId}&page=${page}`,
+    posts: (topicId,postId,page) => `/forum/viewthread.php?fid=${topicId}&tid=${postId}&page=${page}`,
+    login: '/forum/logging.php?action=login&loginsubmit=yes&floatlogin=yes&inajax=1',
+    reply: (topicId,postId) => `/forum/post.php?action=reply&fid=${topicId}&tid=${postId}&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1`
   },
 
   data:{
